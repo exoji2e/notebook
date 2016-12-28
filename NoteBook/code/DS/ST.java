27a26cbdc151024efa571072092df561e200c485
Måns Magnusson <exoji2e@gmail.com>
2016-12-28 21:36:41 +0100

    Add UF, fix ST line overflow

--- NoteBook/code/DS/ST.java
+++ NoteBook/code/DS/ST.java
@@ -1,50 +1,49 @@
 private static class ST {
-  int leftIndex;
-  int rightIndex;
-  int sum;
-  ST leftNode;
-  ST rightNode;
+  int li, ri;
+  int sum; //change to max/min
+  ST lN;
+  ST rN;
 }
 static ST makeSgmTree(int[] A, int l, int r) {
   if(l == r) {
     ST node = new ST();
-    node.leftIndex = l;
-    node.rightIndex = r;
+    node.li = l;
+    node.ri = r;
     node.sum = A[l];
     return node;
   }
   int mid = (l+r)/2;
-  ST leftNode = makeSgmTree(A,l,mid);
-  ST rightNode = makeSgmTree(A,mid+1,r);
+  ST lN = makeSgmTree(A,l,mid);
+  ST rN = makeSgmTree(A,mid+1,r);
   ST root = new ST();
-  root.leftIndex = leftNode.leftIndex;
-  root.rightIndex = rightNode.rightIndex;
-  root.sum = leftNode.sum + rightNode.sum;
-  root.leftNode = leftNode;
-  root.rightNode = rightNode;
+  root.li = lN.li;
+  root.ri = rN.ri;
+  root.sum = lN.sum + rN.sum;
+  root.lN = lN;
+  root.rN = rN;
   return root;
 }
-static int getSum(ST root, int l, int r) {
-  if(root.leftIndex>=l && root.rightIndex<=r)
-    return root.sum;
-  if(root.rightIndex<l || root.leftIndex > r) 
-    return 0;
-  else
-    return getSum(root.leftNode,l,r) + getSum(root.rightNode,l,r);
+static int getSum(ST root, int l, int r) {//max/min
+  if(root.li>=l && root.ri<=r)
+    return root.sum; //max/min
+  if(root.ri<l || root.li > r) 
+    return 0; //minInt/maxInt
+  else //max/min
+    return getSum(root.lN,l,r) + getSum(root.rN,l,r);
 }
-static int updateValueAtIndex(ST root, int index, int newValue) {
+static int update(ST root, int index, int val) {
   int diff = 0;
-  if(root.leftIndex==root.rightIndex && index == root.leftIndex) {
-    diff = newValue-root.sum;
-    root.sum=newValue;
-    return diff;
+  if(root.li==root.ri && index == root.li) {
+    diff = val-root.sum; //max/min
+    root.sum=val; //max/min
+    return diff; //root.max
   }
-  int mid = (root.leftIndex + root.rightIndex) / 2;
+  int mid = (root.li + root.ri) / 2;
   if (index <= mid) {
-    diff= updateValueAtIndex(root.leftNode, index, newValue);
+    diff= update(root.lN, index, val);
   } else {
-    diff= updateValueAtIndex(root.rightNode, index, newValue);
+    diff= update(root.rN, index, val);
   }
-  root.sum+=diff;
-  return diff;
+  root.sum+=diff; //ask other child
+  return diff; //and compute max/min
 }
