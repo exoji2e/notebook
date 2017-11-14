0b1c30a620c8bd49cdbcf6441c1804faf68cb268
Måns Magnusson <exoji2e@gmail.com>
2017-11-14 20:29:08 +0100

    add itertools

--- code/template.py
+++ code/template.py
@@ -1,6 +1,7 @@
 from collections import defaultdict
 from collections import deque
 from collections import Counter
+from itertools import permutations #No repeated elements
 import sys
 sys.setrecursionlimit(1000000)
 
