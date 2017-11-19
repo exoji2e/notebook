256b5dcdd84303b0aac733313664f54a18cb7516
Måns Magnusson <exoji2e@gmail.com>
2017-11-19 19:57:58 +0100

    add small examples to py template

--- code/template.py
+++ code/template.py
@@ -2,6 +2,16 @@ from collections import defaultdict
 from collections import deque
 from collections import Counter
 from itertools import permutations #No repeated elements
-import sys
+import sys, bisect
 sys.setrecursionlimit(1000000)
+# q = deque([0])
+# a = q.popleft()
+# q.append(0)
+
+# a = [1, 2, 3, 3, 4]
+# bisect.bisect(a, 3) == 4
+# bisect.bisect_left(a, 3) == 2
+
+# reversed()
+# sorted()
 
