f8a02152d4c68b762db6c88fa27c0fc7e9f2540d
Måns Magnusson <exoji2e@gmail.com>
2017-11-20 20:24:45 +0100

    fix xgcd and add crt

--- code/Etc/numbertheory.py
+++ code/Etc/numbertheory.py
@@ -5,11 +5,29 @@ def gcd(a, b):
 # where g = x0*a + y0*b
 def xgcd(a, b):
   x0, x1, y0, y1 = 1, 0, 0, 1
-  while a != 0:
-    q, a, n = (a // b, b, a % b)
+  while b != 0:
+    q, a, b = (a // b, b, a % b)
     x0, x1 = (x1, x0 - q * x1)
     y0, y1 = (y1, y0 - q * y1)
   return (a, x0, y0)
+
+def crt(la, ln):
+    assert len(la) == len(ln)
+    for i in range(len(la)):
+        assert 0 <= la[i] < ln[i]
+    prod = 1
+    for n in ln:
+        assert gcd(prod, n) == 1
+        prod *= n
+    lN = []
+    for n in ln:
+        lN.append(prod//n)
+    x = 0
+    for i, a in enumerate(la):
+        print(lN[i], ln[i])
+        _, Mi, mi = xgcd(lN[i], ln[i])
+        x += a*Mi*lN[i]
+    return x % prod
 
 # finds x^e mod m
 def modpow(x, m, e):
