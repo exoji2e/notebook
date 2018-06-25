3f225c277ab23f9cce057022522fa607a01308d6
Måns Magnusson <exoji2e@gmail.com>
2018-06-25 20:08:19 +0200

    fix flow.py, now tested on 1 kattis problem

--- code/Graphs/flow.py
+++ code/Graphs/flow.py
@@ -1,28 +1,41 @@
 class Flow:
     def __init__(self, sz):
-        self.G = [[0]*sz for _ in range(sz)]
-        self.adj = [[] for i in range(sz)]
+        self.G = [defaultdict(int) for _ in range(sz)]
     
     def add_edge(self, i, j, w):
-        G[i][j] = w
-        adj[i].append(j)
-        adj[j].append(i)
+        self.G[i][j] = w
     
-    def dfs(self, s, t, v):
-        if s in v: return 0
-        v.add(s)
-        if s == t: return 10**18 # INF
-        for nxt in adj[s]:
-            push = min(G[s][nxt], self.dfs(nxt, t, v))
-            if push:
-                G[s][nxt] -= push
-                G[nxt][s] += push
-                return push
+    def bfs(self, s, t):
+        vis = {s:s}
+        q = [s]
+        while q:
+            q2 = []
+            for u in q:
+                for v, w in self.G[u].items():
+                    if w and not v in vis:
+                        vis[v] = u
+                        q2.append(v)
+                        if v == t:
+                            return self.reconstruct(s, t, vis)
+            q = q2
         return 0
+
+    def reconstruct(self, s, t, vis):
+        path = [t]
+        push = 10**18
+        while t != s:
+            push = min(push, self.G[vis[t]][t])
+            t = vis[t]
+            path.append(t)
+        for i in range(len(path) - 1):
+            self.G[path[i+1]][path[i]] -= push
+            self.G[path[i]][path[i+1]] += push
+        return push
 
     def max_flow(self, s, t):
         flow = 0
-        while 1:
-            pushed = self.dfs(s, t, set())
+        while True:
+            pushed = self.bfs(s, t)
             if not pushed: break
             flow += pushed
+        return flow
