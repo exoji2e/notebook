e0a47eff08f056db4f279aa7ae2b47f54ae2ad19
Måns Magnusson <exoji2e@gmail.com>
2019-09-24 15:56:31 +0200

    update FW

--- code/DP/floydwarshall.py
+++ code/DP/floydwarshall.py
@@ -1,21 +1,24 @@
-n, m = map(int, input().split())
-INF = 1000000000
-dist = [[INF]*n for _ in range(n)]
-nxt = [[None]*n for _ in range(n)]
-edgs = [tuple(map(int, input().split())) for _ in range(m)]
-for e in edgs:
-    dist[e[0]][e[1]] = e[2]
-    nxt[e[0]][e[1]] = e[1]
-for k in range(n):
-    for i in range(n):
-        for j in range(n):
-            if dist[i][j] > dist[i][k] + dist[k][j]:
-                dist[i][j] = dist[i][k] + dist[k][j]
-                nxt[i][j] = nxt[i][k]
-def path(i, j):
-    if nxt[i][j] == None: return []
-    path = [i]
-    while i != j:
-        i = nxt[i][j]
-        path.append(i)
-    return path
+# Computes distance matrix and next matrix given an edgelist
+def FloydWarshall(n, edges):
+	INF = 1000000000
+	dist = [[INF]*n for _ in range(n)]
+	nxt = [[None]*n for _ in range(n)]
+	for e in edgs:
+		dist[e[0]][e[1]] = e[2]
+		nxt[e[0]][e[1]] = e[1]
+	for k in range(n):
+		for i in range(n):
+			for j in range(n):
+				if dist[i][j] > dist[i][k] + dist[k][j]:
+					dist[i][j] = dist[i][k] + dist[k][j]
+					nxt[i][j] = nxt[i][k]
+	return dist, nxt
+
+# Computes the path from i to j given a nextmatrix
+def path(i, j, nxt):
+	if nxt[i][j] == None: return []
+	path = [i]
+	while i != j:
+		i = nxt[i][j]
+		path.append(i)
+	return path
