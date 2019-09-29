daca62be32aaf112987c33e073ecb3141aa18433
Måns Magnusson <exoji2e@gmail.com>
2019-09-29 15:23:51 +0200

    2sat is now in python

---
+++ code/Graphs/TwoSat.py
@@ -0,0 +1,51 @@
+import sys
+sys.setrecursionlimit(10**5)
+class Sat:
+    def __init__(self, no_vars):
+        self.size = no_vars*2
+        self.no_vars = no_vars
+        self.adj = [[] for _ in range(self.size())]
+        self.back = [[] for _ in range(self.size())]
+    def add_imply(self, i, j):
+        self.adj[i].append(j)
+        self.back[j].append(i)
+    def add_or(self, i, j):
+        self.add_imply(i^1, j)
+        self.add_imply(j^1, i)
+    def add_xor(self, i, j):
+        self.add_or(i, j)
+        self.add_or(i^1, j^1)
+    def add_eq(self, i, j):
+        self.add_xor(i, j^1)
+    
+    def dfs1(self, i):
+        if i in self.marked: return
+        self.marked.add(i)
+        for j in self.adj[i]:
+            self.dfs1(j)
+        self.stack.append(i)
+
+    def dfs2(self, i):
+        if i in self.marked: return
+        self.marked.add(i)
+        for j in self.back[i]:
+            self.dfs2(j)
+        self.comp[i] = self.no_c
+
+    def is_sat(self):
+        self.marked = set()
+        self.stack = []
+        for i in range(self.size):
+            self.dfs1(i)
+        self.marked = set()
+        self.no_c = 0
+        self.comp = [0]*self.size
+        while self.stack:
+            i = self.stack.pop()
+            if i not in self.marked:
+                self.no_c += 1
+                self.dfs2(i)
+        for i in range(self.no_vars):
+            if self.comp[i*2] == self.comp[i*2+1]:
+                return False
+        return True
