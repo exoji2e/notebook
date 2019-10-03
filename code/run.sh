a2775a7c1e87a52d9dc32899e6306579d1e5972a
Måns Magnusson <exoji2e@gmail.com>
2019-10-03 13:44:56 +0200

    add defines in cpp template, and som primes

--- code/run.sh
+++ code/run.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
-# make exacutable: chmod +x run.sh
+# make executable: chmod +x run.sh
 # run: ./run.sh A pypy A.py
-# or
-# ./run.sh A ./a.out
 folder=$1;shift
 for f in $folder/*.in; do
     echo $f
--- code/template.cpp
+++ code/template.cpp
@@ -1,12 +1,20 @@
-#include <stdio.h>
-#include <iostream>
-#include <algorithm>
-#include <vector>
-#include <math.h>
-#include <cmath>
+#include <bits/stdc++.h>
 using namespace std;
+
+#define rep(i, a, b) for(int i = a; i < (b); ++i)
+#define trav(a, x) for(auto& a : x)
+#define sz(x) (int)(x).size()
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef vector<int> vi;
+typedef long long ll;
+ll smod(ll a, ll b){
+    return (a % b + b) % b;
+}
 int main() {
     cout.precision(9);
+    cin.sync_with_stdio(0); cin.tie(0);
+    cin.exceptions(cin.failbit);
     int N; 
     cin >> N;
     cout << 0 << endl;
