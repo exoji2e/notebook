e35d5893609e7de93a3c61fa79e71707fdb7f9a2
Måns Magnusson <exoji2e@gmail.com>
2020-09-29 19:39:20 +0200

    update network flow with scaling maximum capacity

--- code/Graphs/flow.py
+++ code/Graphs/flow.py
@@ -1,31 +1,42 @@
+# used in mincut @ Kattis
 from collections import defaultdict
 class Flow:
     def __init__(self, sz):
-        self.G = [defaultdict(int) for _ in range(sz)]
+        self.G = [
+            defaultdict(int) for _ in range(sz)
+        ] # neighbourhood dict, N[u] = {v_1: cap_1, v_2: cap_2, ...}
+        self.Seen = set() # redundant
     
-    def add_edge(self, i, j, w):
-        self.G[i][j] += w
+    def increase_capacity(self, u, v, cap):
+        """ Increases capacity on edge (u, v) with cap. 
+            No need to add the edge """
+        self.G[u][v] += cap
     
-    def dfs(self, u, FLOW):
-        if u in self.reached: return 0
-        if u == self.T: return FLOW
-        G = self.G
-        self.reached.add(u)
-        for v, w in G[u].items():
-            if w:
-                f = self.dfs(v, min(FLOW, w))
-                if f:
-                    G[u][v] -= f
-                    G[v][u] += f
-                    return f
-        return 0
+    def max_flow(self, source, sink):
+        def dfs(u, hi):
+            G = self.G
+            Seen = self.Seen
+            if u in Seen: return 0
+            if u == sink: return hi
+            
+            Seen.add(u)
+            for v, cap in G[u].items():
+                if cap >= self.min_edge:
+                    f = dfs(v, min(hi, cap))
+                    if f:
+                        G[u][v] -= f
+                        G[v][u] += f
+                        return f
+            return 0
 
-    def max_flow(self, S, T):
         flow = 0
-        self.T = T
-        while True:
-            self.reached = set()
-            pushed = self.dfs(S, float('inf'))
-            if not pushed: break
+        self.min_edge = 2**30 # minimal edge allowed
+        while self.min_edge > 0:
+            self.Seen = set()
+            pushed = dfs(source, float('inf'))
+            if not pushed:
+                self.min_edge //= 2
             flow += pushed
         return flow
+
+
