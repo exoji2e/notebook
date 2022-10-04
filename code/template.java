18f5d7295a8e5d57a6b6146b57390eb4a06d0757
Måns Magnusson <mans.magnusson@itacih.se>
2022-10-04 19:36:56 +0200

    update java template

--- code/template.java
+++ code/template.java
@@ -1,20 +1,89 @@
 import java.util.*;
 import java.io.*;
 public class A {
-    void solve(BufferedReader in) throws Exception {
+    void solve(Kattio io) {
+        
     }
-    int toInt(String s) {return Integer.parseInt(s);}
-    int[] toInts(String s) {
-        String[] a = s.split(" ");
-        int[] o = new int[a.length];
-        for(int i = 0; i<a.length; i++) 
-            o[i] = toInt(a[i]);
-        return o;
+    void run() {
+        Kattio io = new Kattio(System.in, System.out);
+        solve(io);
+        io.flush();
     }
-    public static void main(String[] args) 
-    throws Exception {
-        BufferedReader in = new BufferedReader
-            (new InputStreamReader(System.in));
-        (new A()).solve(in);
+    public static void main(String[] args) {
+        (new A()).run();
+    }
+    class Kattio extends PrintWriter {
+        public Kattio(InputStream i) {
+            super(new BufferedOutputStream(System.out));
+            r = new BufferedReader(new InputStreamReader(i));
+        }
+        public Kattio(InputStream i, OutputStream o) {
+            super(new BufferedOutputStream(o));
+            r = new BufferedReader(new InputStreamReader(i));
+        }
+
+        public boolean hasMoreTokens() {
+            return peekToken() != null;
+        }
+
+        public int getInt() {
+            return Integer.parseInt(nextToken());
+        }
+
+        public double getDouble() { 
+            return Double.parseDouble(nextToken());
+        }
+
+        public long getLong() {
+            return Long.parseLong(nextToken());
+        }
+
+        public String getWord() {
+            return nextToken();
+        }
+
+        private BufferedReader r;
+        private String line;
+        private StringTokenizer st;
+        private String token;
+
+        private String peekToken() {
+            if (token == null) 
+                try {
+                    while (st == null || !st.hasMoreTokens()) {
+                        line = r.readLine();
+                        if (line == null) return null;
+                        st = new StringTokenizer(line);
+                    }
+                    token = st.nextToken();
+                } catch (IOException e) { }
+            return token;
+        }
+
+        private String nextToken() {
+            String ans = peekToken();
+            token = null;
+            return ans;
+        }
+        private String joinRemainder() {
+            ArrayList<String> tokens = new ArrayList<>();
+            while (st.hasMoreTokens()) {
+                tokens.add(st.nextToken());
+            }
+            return String.join(" ", tokens);
+        }
+        public String remainingLine() {
+            if(st != null && st.hasMoreTokens()) {
+                return joinRemainder();
+            }
+            return nextLine();
+        }
+        public String nextLine() {
+            try {
+                return r.readLine();
+            } catch(IOException e) {
+                return null;
+            }
+        }
     }
 }
